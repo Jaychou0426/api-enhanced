656cd2a49fe23bca874438e7ada47f49020a6948
lmc <1922077337@qq.com>
2026-08-05 15:30:39 +0800

    /api/msg/notices 接口新增2个参数配置

--- module/msg_notices.js
+++ module/msg_notices.js
@@ -5,6 +5,8 @@ module.exports = (query, request) => {
   const data = {
     limit: query.limit || 30,
     time: query.lasttime || -1,
+    setNewCount:true,
+    total:true
   }
   return request(`/api/msg/notices`, data, createOption(query, 'weapi'))
 }
